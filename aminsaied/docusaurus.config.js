9a76c10d467d400822c51c9ff64b1cf66e1bb848
amin <amin.saied@gmail.com>
2022-07-05 21:17:25 -0700

    clean comments

--- aminsaied/docusaurus.config.js
+++ aminsaied/docusaurus.config.js
@@ -14,15 +14,10 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/logo.svg',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'aminsaied', // Usually your GitHub org/user name.
-  projectName: 'aminsaied.github.io', // Usually your repo name.
+  organizationName: 'aminsaied',
+  projectName: 'aminsaied.github.io',
   trailingSlash: false,
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
